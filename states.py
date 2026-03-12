165f78f92dff946c6febea1cc16525a91534fb26
IronFeniks <iron.shizo@gmail.com>
2026-03-13 01:38:03 +0400

    Update states.py

--- states.py
+++ states.py
@@ -8,8 +8,6 @@ class AdminStates:
     # Состояния для категорий
     CATEGORY_LIST = 1
     CATEGORY_ADD_NAME = 2
-    CATEGORY_EDIT_NAME = 3
-    CATEGORY_DELETE_CONFIRM = 4
     
     # Состояния для изделий
     PRODUCT_LIST = 10
@@ -18,36 +16,16 @@ class AdminStates:
     PRODUCT_ADD_CATEGORY = 13
     PRODUCT_ADD_PRICE = 14
     PRODUCT_ADD_MULTIPLICITY = 15
-    PRODUCT_EDIT_SELECT = 16
-    PRODUCT_EDIT_FIELD = 17
     PRODUCT_LINK_NODE_SELECT = 18
     PRODUCT_LINK_NODE_QUANTITY = 19
     PRODUCT_LINK_MATERIAL_SELECT = 20
     PRODUCT_LINK_MATERIAL_QUANTITY = 21
-    
-    # Состояния для узлов
-    NODE_LIST = 30
-    NODE_ADD_CODE = 31
-    NODE_ADD_NAME = 32
-    NODE_ADD_CATEGORY = 33
-    NODE_ADD_PRICE = 34
-    NODE_ADD_MULTIPLICITY = 35
-    NODE_EDIT_SELECT = 36
-    NODE_EDIT_FIELD = 37
-    NODE_LINK_MATERIAL_SELECT = 38
-    NODE_LINK_MATERIAL_QUANTITY = 39
     
     # Состояния для материалов
     MATERIAL_LIST = 50
     MATERIAL_ADD_CODE = 51
     MATERIAL_ADD_NAME = 52
     MATERIAL_ADD_CATEGORY = 53
-    MATERIAL_EDIT_SELECT = 54
-    MATERIAL_EDIT_FIELD = 55
-    
-    # Состояния для подтверждения
-    CONFIRM_ACTION = 90
-    WAITING_FOR_AUTH_CODE = 91
 
 def get_user_data(context: ContextTypes.DEFAULT_TYPE, user_id: int) -> dict:
     """Получает или создаёт словарь данных для пользователя"""
